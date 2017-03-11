e10b2c7ece24156f591a78046542424432f3320d
Suriya <s.ravichandran@lakeba.com>
2017-03-11 13:02:15 +0530

    initial commit

---
+++ LeamaSystems.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LeamaSystems.Web.Startup))]
+namespace LeamaSystems.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
